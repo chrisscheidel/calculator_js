f089d05998bc77ad8a9fc3032d117fb899aae295
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-17 22:31:25 -0400

    Create html template and initial operations

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+    return a + b;
+};
+
+function subtract(a,b) {
+    return a - b;
+	
+};
+
+function multiply(a,b) {
+    return a * b;
+};
+
+function divide(a,b) {
+    return a / b;
+};
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+let mapping = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+}
+
+function operate(firstNumber, secondNumber, operator, mapping) {
+    let output = mapping[operater](firstNumber,secondNumber);
+    return output
+};
+
